e9f8a711d0d891d82cc7f2813cf319fe79961e14
jfisher <jfisher@modopayments.com>
2018-03-28 18:02:05 -0500

    added .gitignore

---
+++ base.tf
@@ -0,0 +1,12 @@
+/* This is a multi-line comment. This is a multi-line comment. This is a multi-line comment. This is a multi-line comment. This is a multi-line comment. This is a multi-line comment. */
+provider "aws" {
+  shared_credentials_file = "/Users/jfisher/.aws/credentials"
+  profile    = "jmf-personal"
+  region     = "us-east-1"
+}
+
+# This is a single-line comment.
+resource "aws_instance" "testtfi" {
+  ami           = "ami-0d729a60"
+  instance_type = "t2.micro"
+}
