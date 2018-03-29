a4e2e39cf2935855853b122156a8cb294ff96a45
jfisher <jfisher@modopayments.com>
2018-03-29 09:52:12 -0500

    finished ch 2

--- base.tf
+++ base.tf
@@ -1,12 +1,16 @@
 /* This is a multi-line comment. This is a multi-line comment. This is a multi-line comment. This is a multi-line comment. This is a multi-line comment. This is a multi-line comment. */
 provider "aws" {
   shared_credentials_file = "/Users/jfisher/.aws/credentials"
-  profile    = "jmf-personal"
-  region     = "us-east-1"
+  profile                 = "jmf-personal"
+  region                  = "us-east-1"
 }
 
 # This is a single-line comment.
 resource "aws_instance" "testtfi" {
   ami           = "ami-0d729a60"
   instance_type = "t2.micro"
+}
+
+resource "aws_eip" "testtfi" {
+  instance = "${aws_instance.testtfi.id}"
 }
